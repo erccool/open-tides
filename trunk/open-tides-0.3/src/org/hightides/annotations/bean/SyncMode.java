fbe7d16b692e1dddf596c41bd4514413e4842f34
allanctan <allanctan@d516eca9-d74e-0410-817f-033985a3f49e>
2010-10-07 16:23:53 +0000

    restruture repo

--- trunk/open-tides-0.3/src/org/hightides/annotations/bean/SyncMode.java
+++
@@ -1,7 +0,0 @@
-package org.hightides.annotations.bean;
-
-public enum SyncMode {
-	CREATE, 	// Create the source during sync if not exist. If file exist, no processing performed.
-	REPLACE,	// Overwrites the source duing sync. If file exist, it will be replaced with generated code.
-	RETAIN 		// Keeps the changes and replace generated codes
-}
--- trunk/open-tides-0.3/src/org/opentides/service/BaseService.java
+++
@@ -1,4 +0,0 @@
-package org.opentides.service;
-
-public interface BaseService {
-}
